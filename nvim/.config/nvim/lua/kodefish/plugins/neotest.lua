596e9b86197081730f498cbe3dcdcb10c9603c6f
Danil Ivanov <danil.ivanov@felfel.ch>
2024-07-22 16:25:00 +0200

    neotest better config

--- nvim/.config/nvim/lua/kodefish/plugins/neotest.lua
+++ nvim/.config/nvim/lua/kodefish/plugins/neotest.lua
@@ -16,6 +16,7 @@ return {
 				neotest_python({
 					dap = { justMyCode = false },
 					runner = "pytest",
+                    args = { "-v" },
 					python = "python", -- nvim is already running in the appropriate environment
 				}),
 			},
@@ -26,9 +27,6 @@ return {
 		-- Keybindings
 		vim.keymap.set("n", "<leader>t", "", { desc = "+test" })
 		vim.keymap.set("n", "<leader>tn", neotest.run.run, { desc = "Run Nearest" })
-		vim.keymap.set("n", "<leader>td", function()
-			neotest.run.run({ strategy = "dap" })
-		end, { desc = "Debug Nearest" })
 		vim.keymap.set("n", "<leader>tt", function()
 			neotest.run.run(vim.fn.expand("%"))
 		end, { desc = "Run File" })
@@ -36,11 +34,18 @@ return {
 			neotest.run.run(vim.uv.cwd())
 		end, { desc = "Run Project" })
 		vim.keymap.set("n", "<leader>tl", neotest.run.run_last, { desc = "Run Last" })
-		vim.keymap.set("n", "<leader>ts", neotest.summary.toggle, { desc = "Toggle Summary" })
-		vim.keymap.set("n", "<leader>to", neotest.output_panel.toggle, { desc = "Toggle Ouput" })
+        -- NOTE: The summary is more like a test outline, and the output looks more like a summary, so for mnemonics
+        -- it is easier to switch the two panels
+		vim.keymap.set("n", "<leader>to", neotest.summary.toggle, { desc = "Toggle Outline" })
+		vim.keymap.set("n", "<leader>ts", neotest.output_panel.toggle, { desc = "Toggle Summary" })
 		vim.keymap.set("n", "<leader>tS", neotest.run.stop, { desc = "Stop" })
 		vim.keymap.set("n", "<leader>tw", function()
 			neotest.watch.toggle(vim.fn.expand("%"))
 		end, { desc = "Toggle Watch" })
+
+        -- Debug keybindings (debug nearest 
+		vim.keymap.set("n", "<leader>dn", function()
+			neotest.run.run({ strategy = "dap" })
+		end, { desc = "[D]ebug [N]earest" })
 	end,
 }
