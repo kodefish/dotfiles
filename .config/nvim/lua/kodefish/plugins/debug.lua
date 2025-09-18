3ca1c083acf4f5e242f6d36b04c0d1824bf8f97a
Danil <danilivanov.ivanov@gmail.com>
2025-09-18 22:47:04 +0200

    c# testing and debugging

--- .config/nvim/lua/kodefish/plugins/debug.lua
+++ .config/nvim/lua/kodefish/plugins/debug.lua
@@ -1,80 +1,93 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"theHamsta/nvim-dap-virtual-text",
-		"nvim-neotest/nvim-nio",
-		-- "nvim-telescope/telescope-dap.nvim",
-		"leoluz/nvim-dap-go",
-		"mfussenegger/nvim-dap-python",
-	},
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "theHamsta/nvim-dap-virtual-text",
+        "nvim-neotest/nvim-nio",
+        -- "nvim-telescope/telescope-dap.nvim",
+        "mfussenegger/nvim-dap-python", -- python
+    },
+    config = function()
+        local dap, dapui = require("dap"), require("dapui")
 
-		-- Setup dap-ui
-		dapui.setup()
+        -- Setup dap-ui
+        dapui.setup()
 
-		-- Hook dapui into dap events
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+        -- Hook dapui into dap events
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
 
         -- Setup virtual text
         local dap_virtual_text = require("nvim-dap-virtual-text")
         dap_virtual_text.setup({
-            virt_text_pos = 'eol'
+            virt_text_pos = "eol",
         })
 
-		-- Keymaps
-		vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Dap UI" })
-		vim.keymap.set({ "n", "v" }, "<leader>de", dapui.eval, { desc = "Eval" })
+        -- Keymaps
+        vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Dap UI" })
+        vim.keymap.set({ "n", "v" }, "<leader>de", dapui.eval, { desc = "Eval" })
 
-		-- Configure adapters
-		local dap_go = require("dap-go")
-		dap_go.setup({
-			delve = {
-				-- delve installed via mason to $XDG_DATA_HOME/nvim/mason/bin/
-				path = vim.fn.stdpath("data") .. "/mason/bin" .. "/dlv",
-			},
-		})
-
-		local dap_python = require("dap-python")
+        -- Configure adapters
+        -- Tools installed with Mason can be found at
+        -- vim.fn.stdpath("data") .. "/mason/bin" .. "/<executable>"
+        -- Python
+        local dap_python = require("dap-python")
         dap_python.setup("python")
 
+        -- C# / Dotnet
+        -- Point dotnet dap to netcoredgb
+        local netcoredbg_bin = "/opt/homebrew/bin/netcoredbg" -- adjust if needed
+        dap.adapters.netcoredbg = {
+            type = "executable",
+            command = netcoredbg_bin,
+            args = { "--interpreter=vscode" },
+        }
+        dap.adapters.coreclr = dap.adapters.netcoredbg
+        dap.configurations.cs = {
+            {
+                type = "coreclr",
+                name = "launch - netcoredbg",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/net9.0/", "file")
+                end,
+            },
+        }
 
-		-- Keymaps
-		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-		vim.keymap.set("n", "<leader>dB", function()
-			dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-		end, { desc = "Conditional breakpoint" })
-		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
-		vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run to cursor" })
-		vim.keymap.set("n", "<leader>dg", dap.goto_, { desc = "Go to line (no execute)" })
-		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
-		vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Down" })
-		vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Up" })
-		vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Run Last" })
-		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
-		vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "Step Out" })
-		vim.keymap.set("n", "<leader>dp", dap.pause, { desc = "Pause" })
-		vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle REPL" })
-		vim.keymap.set("n", "<leader>ds", dap.session, { desc = "Session" })
-		vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Terminate" })
+        -- Keymaps
+        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+        vim.keymap.set("n", "<leader>dB", function()
+            dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end, { desc = "Conditional breakpoint" })
+        vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+        vim.keymap.set("n", "<leader>dC", dap.run_to_cursor, { desc = "Run to cursor" })
+        vim.keymap.set("n", "<leader>dg", dap.goto_, { desc = "Go to line (no execute)" })
+        vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
+        vim.keymap.set("n", "<leader>dj", dap.down, { desc = "Down" })
+        vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Up" })
+        vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "Run Last" })
+        vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
+        vim.keymap.set("n", "<leader>dO", dap.step_out, { desc = "Step Out" })
+        vim.keymap.set("n", "<leader>dp", dap.pause, { desc = "Pause" })
+        vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle REPL" })
+        vim.keymap.set("n", "<leader>ds", dap.session, { desc = "Session" })
+        vim.keymap.set("n", "<leader>dt", dap.terminate, { desc = "Terminate" })
 
-		-- Visuals
-		vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "", numhl = "", linehl = "" })
-		vim.fn.sign_define("DapStopped", { text = "→", texthl = "", numhl = "", linehl = "" })
+        -- Visuals
+        vim.fn.sign_define("DapBreakpoint", { text = "●", texthl = "", numhl = "", linehl = "" })
+        vim.fn.sign_define("DapStopped", { text = "→", texthl = "", numhl = "", linehl = "" })
 
-		-- Load launch.json
+        -- Load launch.json
         require("dap.ext.vscode").load_launchjs()
-	end,
+    end,
 }
--- .config/nvim/lua/kodefish/plugins/neotest.lua
+++ .config/nvim/lua/kodefish/plugins/neotest.lua
@@ -1,52 +1,78 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-neotest/neotest-python",
-	},
-	config = function()
-		local neotest_python = require("neotest-python")
-		local neotest = require("neotest")
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-python",
+        "Issafalcon/neotest-dotnet",
+    },
+    config = function()
+        local neotest_python = require("neotest-python")
+        local neotest_dotnet = require("neotest-dotnet")
+        local neotest = require("neotest")
 
-		neotest.setup({
-			adapters = {
-				neotest_python({
-					dap = { justMyCode = false },
-					runner = "pytest",
-					args = { "-vv", "--color=no" },
-                     -- nvim is already running in the appropriate environment (e.g. devcontainer / virtual env)
-					python = "python",
-				}),
-			},
-			status = { virtual_text = true },
-			output = { open_on_run = true },
-		})
+        neotest.setup({
+            log_level = 1,
+            adapters = {
+                neotest_python({
+                    dap = { justMyCode = false },
+                    runner = "pytest",
+                    args = { "-vv", "--color=no" },
+                    -- nvim is already running in the appropriate environment (e.g. devcontainer / virtual env)
+                    python = "python",
+                }),
+                neotest_dotnet({
+                    dap = {
+                        -- Extra arguments for nvim-dap configuration
+                        -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+                        args = { justMyCode = false },
+                        -- Enter the name of your dap adapter, the default value is netcoredbg
+                        adapter_name = "netcoredbg",
+                    },
+                    dotnet_additional_args = {
+                        "--verbosity detailed",
+                    },
+                    discovery_root = "solution",
+                }),
+            },
+            status = { virtual_text = true },
+            output = { open_on_run = true },
+        })
 
-		-- Keybindings
-		vim.keymap.set("n", "<leader>t", "", { desc = "+test" })
-		vim.keymap.set("n", "<leader>tn", neotest.run.run, { desc = "Run Nearest" })
-		vim.keymap.set("n", "<leader>tt", function()
-			neotest.run.run(vim.fn.expand("%"))
-		end, { desc = "Run File" })
-		vim.keymap.set("n", "<leader>tT", function()
-			neotest.run.run(vim.uv.cwd())
-		end, { desc = "Run Project" })
-		vim.keymap.set("n", "<leader>tl", neotest.run.run_last, { desc = "Run Last" })
-		-- NOTE: The summary is more like a test outline, and the output looks more like a summary, so for mnemonics
-		-- it is easier to switch the two panels
-		vim.keymap.set("n", "<leader>to", neotest.summary.toggle, { desc = "Toggle Outline" })
-		vim.keymap.set("n", "<leader>ts", neotest.output_panel.toggle, { desc = "Toggle Summary" })
-		vim.keymap.set("n", "<leader>tS", neotest.run.stop, { desc = "Stop" })
-		vim.keymap.set("n", "<leader>tw", function()
-			neotest.watch.toggle(vim.fn.expand("%"))
-		end, { desc = "Toggle Watch" })
+        -- Keybindings
+        vim.keymap.set("n", "<leader>t", "", { desc = "+test" })
+        vim.keymap.set("n", "<leader>tn", neotest.run.run, { desc = "Run Nearest" })
+        vim.keymap.set("n", "<leader>tt", function()
+            neotest.run.run(vim.fn.expand("%"))
+        end, { desc = "Run File" })
+        vim.keymap.set("n", "<leader>tT", function()
+            neotest.run.run(vim.uv.cwd())
+        end, { desc = "Run Project" })
+        vim.keymap.set("n", "<leader>tl", neotest.run.run_last, { desc = "Run Last" })
+        -- NOTE: The summary is more like a test outline, and the output looks more like a summary, so for mnemonics
+        -- it is easier to switch the two panels
+        vim.keymap.set("n", "<leader>to", neotest.summary.toggle, { desc = "Toggle Outline" })
+        vim.keymap.set("n", "<leader>ts", neotest.output_panel.toggle, { desc = "Toggle Summary" })
+        vim.keymap.set("n", "<leader>tS", neotest.run.stop, { desc = "Stop" })
+        vim.keymap.set("n", "<leader>tw", function()
+            neotest.watch.toggle(vim.fn.expand("%"))
+        end, { desc = "Toggle Watch" })
 
-		-- Debug keybindings (debug nearest
-		vim.keymap.set("n", "<leader>td", function()
-			neotest.run.run({ strategy = "dap" })
-		end, { desc = "Debug Nearest Test" })
-	end,
+        -- Debug keybindings (debug nearest)
+        vim.keymap.set("n", "<leader>td", function()
+            neotest.run.run({ strategy = "dap" })
+        end, { desc = "Debug Nearest Test" })
+
+        local function debug_nearest_wait()
+            local prev = vim.env.VSTEST_HOST_DEBUG
+            vim.env.VSTEST_HOST_DEBUG = "1" -- testhost pauses for debugger
+            neotest.run.run({ strategy = "dap" })
+            vim.defer_fn(function()
+                vim.env.VSTEST_HOST_DEBUG = prev
+            end, 2000)
+        end
+        vim.keymap.set("n", "<leader>tD", debug_nearest_wait, { desc = "Debug Nearest (wait)" })
+    end,
 }
