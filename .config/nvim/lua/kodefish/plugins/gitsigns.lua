ea443393695a6602ddc7d2560bbcfa2d80a7304a
Danil <danilivanov.ivanov@gmail.com>
2025-09-19 10:20:11 +0200

    gitsigns ignore whitespace change at eol

--- .config/nvim/lua/kodefish/plugins/gitsigns.lua
+++ .config/nvim/lua/kodefish/plugins/gitsigns.lua
@@ -1,17 +1,21 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			local gitsigns = require("gitsigns")
-			gitsigns.setup()
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            local gitsigns = require("gitsigns")
+            gitsigns.setup({
+                diff_opts = {
+                    ignore_whitespace_change_at_eol = true,
+                },
+            })
 
-			vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "Git preview hunk" })
-			vim.keymap.set(
-				"n",
-				"<leader>gb",
-				"<cmd>Gitsigns toggle_current_line_blame<cr>",
-				{ desc = "Git blame toggle " }
-			)
-		end,
-	},
+            vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<cr>", { desc = "Git preview hunk" })
+            vim.keymap.set(
+                "n",
+                "<leader>gb",
+                "<cmd>Gitsigns toggle_current_line_blame<cr>",
+                { desc = "Git blame toggle " }
+            )
+        end,
+    },
 }
