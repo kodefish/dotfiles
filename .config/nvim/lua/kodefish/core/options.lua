d8258ab48af9004fb26be20bf6443f07456e3c88
Danil <danilivanov.ivanov@gmail.com>
2025-12-17 11:08:53 +0100

    [nvim] formatting + add gh action filetype + gh action linting

--- .config/nvim/lua/kodefish/core/options.lua
+++ .config/nvim/lua/kodefish/core/options.lua
@@ -3,16 +3,16 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 
-opt.number = true -- show absolute line number on cursor line
+opt.number = true         -- show absolute line number on cursor line
 
 -- tabs & indentation (4 spaces for tab)
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from previous line when starting a new one
+opt.expandtab = true   -- expand tab to spaces
+opt.autoindent = true  -- copy indent from previous line when starting a new one
 opt.smartindent = true -- smart indent when startin a new line
-opt.scrolloff = 8 -- keep padding
+opt.scrolloff = 8      -- keep padding
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
@@ -25,13 +25,13 @@ opt.colorcolumn = "121"
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-insensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-insensitive
 
 -- appearance
 if vim.env.COLORTERM == "truecolor" then
-	vim.o.termguicolors = true -- turn on termguicolors
+    vim.o.termguicolors = true -- turn on termguicolors
 end
-opt.signcolumn = "yes" -- show sign column so text doesn't shift
+opt.signcolumn = "yes"      -- show sign column so text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -50,21 +50,24 @@ opt.autoread = true
 opt.mouse = "a"
 
 vim.g.clipboard = {
-	name = "OSC 52",
-	copy = {
-		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-	},
-	paste = {
-		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-	},
+    name = "OSC 52",
+    copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+        ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
 }
 
 -- Custom filetypes
 vim.filetype.add({
-	pattern = {
-		[".*yml.tmpl"] = "yaml",
-		[".*yaml.tmpl"] = "yaml",
-	},
+    pattern = {
+        [".*yml.tmpl"] = "yaml",
+        [".*yaml.tmpl"] = "yaml",
+        [".*yml.jinja"] = "yaml",
+        [".*yaml.jinja"] = "yaml",
+        [".*/.github/workflows/.*%.ya?ml"] = "yaml.ghaction",
+    },
 })
--- .config/nvim/lua/kodefish/plugins/nvim-lint.lua
+++ .config/nvim/lua/kodefish/plugins/nvim-lint.lua
@@ -1,14 +1,15 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = {
-		"BufReadPre",
-		"BufNewFile",
-	},
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
     config = function()
         local lint = require("lint")
 
         lint.linters_by_ft = {
             -- python = { "mypy" }
+            ["yaml.ghaction"] = { "actionlint" },
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
